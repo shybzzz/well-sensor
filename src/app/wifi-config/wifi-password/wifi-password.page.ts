17664665b9fae3df2b7e4f51a0810d74512fcde7
shybzzz <rostykrom@yahoo.com>
2018-10-04 09:59:16 +0300

    some more fixes to wifi pwd page

--- src/app/wifi-config/wifi-password/wifi-password.page.ts
+++ src/app/wifi-config/wifi-password/wifi-password.page.ts
@@ -1,5 +1,8 @@
+import { HotspotNetwork } from '@ionic-native/hotspot/ngx';
 import { WifiConfigService } from './../wifi-config.service';
 import { Component, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs/internal/Subject';
 
 @Component({
   selector: 'app-wifi-password',
@@ -8,10 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WifiPasswordPage implements OnInit {
 
+  destroyed$ = new Subject();
+  network: HotspotNetwork;
+
   constructor(public wifiConfig: WifiConfigService) { }
 
   ngOnInit() {
-
+    this.wifiConfig.selectedNetwork$.pipe(takeUntil(this.destroyed$)).subscribe((n) => {
+      this.network = n;
+    });
   }
 
 }
