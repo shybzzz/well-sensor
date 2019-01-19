04485641971650a98a7dcdfd9c7a5504705173b3
shybzzz <rostykrom@yahoo.com>
2019-01-19 08:47:21 +0200

    correct redirection from home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
+import { MqttConnectionService } from '../../services/mqtt-connection.service';
+import { SubscriptionService } from '../../services/subscription.service';
+import { MqttConnectionState } from 'ngx-mqtt';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'],
+  providers: [SubscriptionService]
 })
 export class HomePage implements OnInit {
   destroy$ = new Subject();
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private mqttConnectionService: MqttConnectionService,
+    private subscriptionService: SubscriptionService
+  ) {}
 
-  ngOnInit(): void {}
-
-  ionViewDidEnter() {
-    this.router.navigate(['/device-list']);
+  ngOnInit(): void {
+    this.subscriptionService
+      .takeUntilDestroyed(this.mqttConnectionService.state$)
+      .subscribe(state =>
+        this.router.navigate([
+          state === MqttConnectionState.CONNECTED ? 'device-list' : 'new-mqtt'
+        ])
+      );
   }
+
+  ionViewDidEnter() {}
 }
